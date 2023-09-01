f49afba0ab163f20837b138554d1958fac73871a
gautam07best <123300795+gautam07best@users.noreply.github.com>
2023-09-01 17:14:22 +0530

    Add files via upload

---
+++ corephp/php-operators/php_operators.php
@@ -0,0 +1,32 @@
+<!-- 
+operators : operators are used to operand some actions there we used operators
+   types of operator :
+
+   a) airtmatic operator :
+         ex: +, - , *, / , % etc
+   
+   b) assignment operator :
+         ex: =, == , !=  etc       
+
+   c) comparisions operator :
+         ex: ===, > , < , >= , <=  etc
+
+   d) logical operator :
+         ex: && and , or ||, not ! etc
+
+   e) sorthand operator 
+   
+        ex: +=, -=, *=, /=, %= 
+
+    f) ternary operator 
+    
+         ex: ?:
+    
+    g) concatenate string operator 
+    
+        ex: .
+        
+    h) increment/decrement operator
+
+        ex: ++ , --
+ -->
---
+++ corephp/php-string/what is php string.php
@@ -0,0 +1,14 @@
+<?php 
+/* what is php string ?
+Def : string is a set of character inside of "" and '' i.e called string.
+
+$name="brijesh kumar pandey";
+$name11='my name is mona chotaliya';
+
+*/
+$name="brijesh kumar pandey";
+$name1='my name is mona chotaliya';
+echo var_dump($name)."<br>";
+echo var_dump($name1)."<br>";
+
+?>

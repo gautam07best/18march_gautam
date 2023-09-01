53ca3a48e2f4b3e7e274184e8f7263c138ad9ce6
gautam07best <123300795+gautam07best@users.noreply.github.com>
2023-09-01 17:10:28 +0530

    Add files via upload

---
+++ corephp/php-errors/parse_error/parse.php
@@ -0,0 +1,9 @@
+<?php 
+// when we mistake with syntax error there parse error will be generated
+// $name="Brijesh" // terminate or ; colon mistake so parse error generated
+// echo $name;
+
+$name="Brijesh;
+echo $name;
+
+?>

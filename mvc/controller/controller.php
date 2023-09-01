fe239dda4e1584a66852cdce53f84cd71f59d3b9
gautam07best <123300795+gautam07best@users.noreply.github.com>
2023-09-01 17:19:06 +0530

    Add files via upload

---
+++ mvc/controller/controller.php
@@ -0,0 +1,23 @@
+<?php 
+require_once("../model/model.php");
+class controller extends model
+{
+    public function __construct()
+    {
+    //print your name
+    // $name="hello Gautam";
+    // echo $name;   
+
+    // w.a.p to print your name using mvc
+    // if(isset($_POST["chk"]))
+    // {
+    //     $name=$_POST["uname"];
+    //     echo "<h3 align='center'>My name is :$name</h3>";
+    // }
+
+    
+    }
+}
+$obj=new controller;
+
+?>
